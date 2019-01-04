29a950194e4bb769a4e56acc276e8063e295fda3
Stefan Pantic <stefanpantic13@gmail.com>
2019-01-04 21:30:29 +0100

    Implemented operation hierarchy

---
+++ fotoshopic/headers/image_operations.h
@@ -0,0 +1,58 @@
+#pragma once
+
+
+#include "image.h"
+#include "abstract_operation.h"
+
+
+namespace fs::ops
+{
+
+	/*
+	* @brief Class that implements mirror operation.
+	*/
+	class MirrorOperation : public AbstractOperation
+	{
+		public:
+			void apply(Image &img) override;
+			void invert(Image &img) override;
+	};
+
+
+	/*
+	* @brief Class that implements rotations.
+	*/
+	class RotateLeftOperation : public AbstractOperation
+	{
+		public:
+			void apply(Image &img) override;
+			void invert(Image &img) override;
+	};
+
+
+	/*
+	* @brief Class that implements rotations.
+	*/
+	class RotateRightOperation : public AbstractOperation
+	{
+		public:
+			void apply(Image &img) override;
+			void invert(Image &img) override;
+	};
+
+
+	/*
+	* @brief Class that implements resizing.
+	*/
+	class ResizeOperation : public AbstractOperation
+	{
+		public:
+			explicit ResizeOperation(int width, int height);
+			void apply(Image &img) override;
+			void invert(Image &img) override;
+		private:
+			int m_width, m_height;
+			Image m_new, m_old;
+	};
+
+} // namespace fs::ops
