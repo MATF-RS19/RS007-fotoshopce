46430af7a06fea475ee800fe65da81a96e1aac94
Dijana <dijanaz1996@gmail.com>
2019-01-09 20:47:27 +0100

    Changed theme

--- fotoshopic/src/main.cpp
+++ fotoshopic/src/main.cpp
@@ -5,6 +5,11 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
+	QFile File(":/icons/QTDark.stylesheet");
+	File.open(QFile::ReadOnly);
+	QString StyleSheet = QLatin1String(File.readAll());
+
+	qApp->setStyleSheet(StyleSheet);
 	MainWindow w;
 	w.show();
 
